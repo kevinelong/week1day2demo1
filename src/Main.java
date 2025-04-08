f3ce83ee606d84bce75fea5f6b3bdb58de03b862
Kevin Ernest Long <kevinelong@gmail.com>
2025-04-08 16:10:54 -0700

    traditional "for" loop with index example output

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import java.util.Arrays;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         String letters = "ABC \t abc \r\n 123!";
@@ -23,3 +19,28 @@ public class Main {
         }
     }
 }
+
+/* EXPECTED OUTPUT
+ABC 	 abc
+ 123!
+0 'A'=65 41 1000001
+1 'B'=66 42 1000010
+2 'C'=67 43 1000011
+3 ' '=32 20 100000
+4 '	'=9 9 1001
+5 ' '=32 20 100000
+6 'a'=97 61 1100001
+7 'b'=98 62 1100010
+8 'c'=99 63 1100011
+9 ' '=32 20 100000
+'=13 d 1101
+11 '
+'=10 a 1010
+12 ' '=32 20 100000
+13 '1'=49 31 110001
+14 '2'=50 32 110010
+15 '3'=51 33 110011
+16 '!'=33 21 100001
+
+Process finished with exit code 0
+*/
